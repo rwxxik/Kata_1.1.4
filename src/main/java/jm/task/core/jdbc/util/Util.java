066c71b854b2f6fe1f2ce88869678db0bfa87f39
a <rwxxik@gmail.com>
2023-04-19 23:03:25 +0300

    Test commit and push

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,16 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+     private static final String DB_URL = "jdbc:mysql://localhost:3306/myDbTest";
+     private static final String DB_USERNAME = "root";
+     private static final String DB_PASSWORD = "root";
+     public static Connection connection;
+     public static Statement statement;
+
+
+
 }
